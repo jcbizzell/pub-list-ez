d7c86a472a19eace316691acf78902b96ab51b42
Josh Bizzell <jcbizzell@gmail.com>
2018-04-16 15:04:28 -0400

    Added article view and CSS for the site

--- publistez/urls.py
+++ publistez/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.pubs_list, name='pubs_list'),
+    url(r'^article/(?P<pk>\d+)/$', views.article_detail, name='article_detail'),
 ]
--- publistez/views.py
+++ publistez/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Article
 
-def post_list(request):
+def pubs_list(request):
     articles = Article.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'publistez/post_list.html', {'articles': articles})
+    return render(request, 'publistez/pubs_list.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'publistez/article_detail.html', {'article': article})
 
