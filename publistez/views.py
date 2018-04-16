06dda855fe98c125d4485e78f41a5b5166194d74
Josh Bizzell <jcbizzell@gmail.com>
2018-04-16 14:38:23 -0400

    Modified template to show articles from database.

--- publistez/views.py
+++ publistez/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Article
 
 def post_list(request):
-    return render(request, 'publistez/post_list.html', {})
+    articles = Article.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'publistez/post_list.html', {'articles': articles})
 
